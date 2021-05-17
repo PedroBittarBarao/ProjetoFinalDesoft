e5fb95f27b486631712bfed02453ba55ff465f1f
Pedro Bittar Barão <pedrobaraocontas@gmail.com>
2021-05-17 13:40:48 -0300

    Criação de funções main e draw window

--- Jogo.py
+++ Jogo.py
@@ -1,1 +1,26 @@
 import pygame
+
+WIDTH, HEIGHT = 800, 720
+WINDOW=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Nome')
+
+FPS = 60 
+
+def draw_window():
+    WINDOW.fill((255,255,255))
+    pygame.display.update()
+
+def main():
+    clock=pygame.time.Clock()
+    game=True
+    while game:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                game=False
+
+        draw_window()
+
+    pygame.QUIT
+
+main()
