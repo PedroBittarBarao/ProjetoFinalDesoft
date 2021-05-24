85e0523aa9a4c92c2b3a17c6f17f96cddd00f347
Pedro Bittar Barão <pedrobaraocontas@gmail.com>
2021-05-24 15:56:20 -0300

    Criação da mecanica para bat e blocos

--- Jogo.py
+++ Jogo.py
@@ -1,17 +1,24 @@
 import pygame
-import Classes
+from funcoes import setup_blocks,setup_bat,setup_window,update_bat,update_blocks,WINDOW
 import config
+from config import BLOCK_HEIGHT,BLOCK_WIDTH, WIDTH
 import Assets
-from Assets import load_assets,GAME_FONT,BACKGROUND
+from Assets import BAT_IMG, load_assets,GAME_FONT,BACKGROUND,BAT_HEIGHT,BAT_WIDTH,BLOCK_IMG_BLU,BLOCK_IMG_YLW,BLOCK_IMG_GRN,BLOCK_IMG_RED
 
 
-WINDOW=pygame.display.set_mode((config.WIDTH,config.HEIGHT))
+
 pygame.display.set_caption('Nome')
+BAT_POS=config.BAT_POS_0
+keys_down = {}
 
-def draw_window():
+   
+
+def update_window(event):
     WINDOW.fill((255,255,255))
     assets=Assets.load_assets()
     WINDOW.blit(assets[BACKGROUND], (0, 0))
+    update_bat(BAT_POS,event)
+    update_blocks(setup_blocks())
     pygame.display.update()
 
 def main():
@@ -19,13 +26,13 @@ def main():
     pygame.font.init()
     clock=pygame.time.Clock()
     game=True
+    setup_window()
     while game:
         clock.tick(config.FPS)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game=False
-
-        draw_window()
+            update_window(event)
 
     pygame.QUIT
 
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
 FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
 
 #Janela
-WIDTH = 400 # Largura 
+WIDTH = 420 # Largura 
 HEIGHT = 600 # Altura 
 FPS = 60 # Frames por segundo
 
@@ -16,11 +16,12 @@ BLOCK_WIDTH=28
 BLOCK_HEIGHT=14
 BALL_WIDTH=12
 BALL_HEIGHT=12
-BAT_WIDTH=26
-BAT_HEIGHT=12
+BAT_WIDTH=50
+BAT_HEIGHT=10
 
 #estados
 INIT = 0
 GAME = 1
 QUIT = 2
 
+BAT_POS_0=[WIDTH/2-BLOCK_WIDTH,550]
