be87cc454b1187a2e95606c1c796fae2513f7f52
Mia Machado <mariaima@al.insper.edu.br>
2021-05-28 16:32:18 -0300

    Organizando  + comentário sobre a title screen.

--- Assets.py
+++ Assets.py
@@ -1,20 +1,21 @@
-# Arquivo para carregar assets
+#Arquivo para carregar assets
 import pygame
 import os
-from config import BLOCK_WIDTH,BLOCK_HEIGHT,BALL_HEIGHT,BAT_WIDTH,BAT_HEIGHT,BALL_WIDTH,IMG_DIR,FNT_DIR,SND_DIR
+from config import BLOCK_WIDTH,BLOCK_HEIGHT, BALL_WIDTH,BALL_HEIGHT, BAT_WIDTH,BAT_HEIGHT, IMG_DIR,SND_DIR,FNT_DIR
 
 BACKGROUND='background'
 BLOCK_IMG_RED='block_img_red'
 BLOCK_IMG_GRN='block_img_grn'
 BLOCK_IMG_BLU='block_img_blu'
 BLOCK_IMG_YLW='block_img_ylw'
-BAT_IMG='bat_img'
 BALL_IMG='ball_img'
+BAT_IMG='bat_img'
 GAME_FONT='game_font'
 TITLE_SCREEN='title_screen'
 
 def load_assets():
-    assets={}
+    
+    assets = {}
     assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'background.jpg')).convert()
     assets[BLOCK_IMG_RED] = pygame.image.load(os.path.join(IMG_DIR, 'Bloco Vermelho.png')).convert()
     assets[BLOCK_IMG_GRN] = pygame.image.load(os.path.join(IMG_DIR, 'Bloco Verde.png')).convert()
@@ -23,7 +24,9 @@ def load_assets():
     assets[BALL_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'ball_red.png')).convert()
     assets[BAT_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'bat_blue.png')).convert()
     assets[GAME_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'ArcadeNormal-ZDZ.ttf'), 28)
+    #INCLUIR IMAGEM NA PASTA IMG
     assets[TITLE_SCREEN] = pygame.image.load(os.path.join(IMG_DIR, 'Title_screen_placeholder.png')).convert()
+    
     #pygame.mixer.music.load(os.path.join(SND_DIR, 'TBD'))
     #pygame.mixer.music.set_volume(0.4)
 
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# Arquivo com configurações e constantes
+#Arquivo com configurações e constantes
 from os import path
 
-#caminhos
+#Caminhos
 IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
 SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
 FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
@@ -11,7 +11,7 @@ WIDTH = 420 # Largura
 HEIGHT = 600 # Altura 
 FPS = 60 # Frames por segundo
 
-#tamanhos
+#Tamanhos
 BLOCK_WIDTH=28
 BLOCK_HEIGHT=14
 BALL_WIDTH=8
@@ -19,16 +19,16 @@ BALL_HEIGHT=8
 BAT_WIDTH=50
 BAT_HEIGHT=10
 
-#estados
+#Estados
 INIT = 0
 GAME = 1
 QUIT = 2
 
-#posições
-BAT_POS_0=[WIDTH/2-BLOCK_WIDTH,550] #posição inicial 'bat'(horizontal,vertical)
-BALL_POS_0=[WIDTH/2-BLOCK_WIDTH,535] #posição inicial da bola(horizontal,vertical)
+#Posições
+BALL_POS_0 = [WIDTH/2-BLOCK_WIDTH,535] #posição inicial da bola(horizontal,vertical)
+BAT_POS_0 = [WIDTH/2-BLOCK_WIDTH,550] #posição inicial 'bat'(horizontal,vertical)
 
-#velocidades iniciais da bola
-BALL_SPEED_VERT_BASE=-10
-BALL_SPEED_HOR_BASE=-10
+#Velocidades iniciais da bola
+BALL_SPEED_HOR_BASE = -10
+BALL_SPEED_VERT_BASE = -10
 
