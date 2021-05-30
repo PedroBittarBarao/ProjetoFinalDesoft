b2782c7dfa57881f5a262d6272235930493fc373
Pedro Bittar Barão <pedrobaraocontas@gmail.com>
2021-05-30 10:59:07 -0300

    Adição de placar e execução mais rápida do setup

--- Jogo.py
+++ Jogo.py
@@ -10,10 +10,10 @@ from Assets import BAT_IMG, load_assets,GAME_FONT,BACKGROUND,BAT_HEIGHT,BAT_WIDT
 pygame.display.set_caption('Nome')
 BAT_POS=config.BAT_POS_0
 keys_down = {}
-
+SCORE=0
    
 
-def update_window(event,lista_current_speed,ball_img_rect,bat_img_rect,blocks_rect,block_img_blu_scale,block_keys): # puxa funções de atualização e atualiza a tela
+def update_window(event,lista_current_speed,ball_img_rect,bat_img_rect,blocks_rect,block_img_blu_scale,block_keys,SCORE): # puxa funções de atualização e atualiza a tela
     WINDOW.fill((255,255,255))
     assets=Assets.load_assets()
     WINDOW.blit(assets[BACKGROUND], (0, 0))
@@ -21,6 +21,9 @@ def update_window(event,lista_current_speed,ball_img_rect,bat_img_rect,blocks_re
     update_speed(lista_current_speed,ball_img_rect,bat_img_rect,blocks_rect,block_keys)
     update_blocks([block_img_blu_scale,blocks_rect,block_keys])
     update_ball(event,lista_current_speed)
+    SCORE+=len(block_keys)-len(blocks_rect.keys())
+    text1 = assets[GAME_FONT].render('SCORE :{} '.format(SCORE), True, (0, 0, 0))
+    WINDOW.blit(text1, (10, 570))
     pygame.display.update()
 
 def main():
@@ -28,18 +31,19 @@ def main():
     pygame.font.init()
     clock=pygame.time.Clock()
     game=True
-    lista_current_speed=setup_window()[0]
-    ball_img_rect=setup_window()[1]
-    bat_img_rect=setup_window()[2]
-    block_keys=setup_window()[5]
-    blocks_rect=setup_window()[3]
-    block_img_blu_scale=setup_window()[4]
+    lista_setup=setup_window()
+    lista_current_speed=lista_setup[0]
+    ball_img_rect=lista_setup[1]
+    bat_img_rect=lista_setup[2]
+    block_keys=lista_setup[5]
+    blocks_rect=lista_setup[3]
+    block_img_blu_scale=lista_setup[4]
     while game:
         clock.tick(config.FPS)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game=False
-        update_window(event,lista_current_speed,ball_img_rect,bat_img_rect,blocks_rect,block_img_blu_scale,block_keys)
+        update_window(event,lista_current_speed,ball_img_rect,bat_img_rect,blocks_rect,block_img_blu_scale,block_keys,SCORE)
 
     pygame.QUIT
 
